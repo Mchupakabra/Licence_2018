93369201e672266aed308193b65e2ead31516778
Mustapha <Mustapha@Mustapha-PC>
2018-03-25 13:48:40 +0100

    Initialisaion du répertoire et création d'un projet vide,

---
+++ Mus_project/src/mus_project/Mus_project.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mus_project;
+
+/**
+ *
+ * @author Mustapha
+ */
+public class Mus_project {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
